81caf2f54a2e0e38c01d38c62dc1b9ce7f4e875d
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-11-09 08:35:26 -0800

    Added   dist/main.bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+   clean: true,
   },
 };
