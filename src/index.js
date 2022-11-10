b5f23b8d04c9f1a52f9e32a03a2346810e996d1a
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-11-10 00:44:53 -0800

    modified js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './styles/style.css';
-import UI  from './modules/ui.js'
+import UI from './modules/ui.js';
 import Todo from './modules/todo.js';
 
-document.addEventListener(
-  'DOMContentLoaded',UI.displayTodo);
-  const bOOK = New Todo(desc,completed,id);
-  UI.addBookTolist(todo);
+document.addEventListener('DOMContentLoaded', UI.displayTodo);
 
+// eslint-disable-next-line no-undef
+
+// eslint-disable-next-line no-undef, no-unused-vars
+const book = new Todo(desc, completed, id);
+
+UI.addBookToList(Todo);
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,37 +1,40 @@
-const UI = {
- displayTodo() {
-    const toDos = [
-    {
-    desc: 'cook breakfast',
-    id: '1', 
-  },
-  {
-    desc: 'Attend class',
-    id: '2',
-  },
-  {
-    desc: 'Revise books',
-    id: '3',
-  },
-  {
-    desc: 'visit granny',
-    id: '4',
-  },
-];
+class UI {
+  static displayTodo() {
+    const storedTodo = [
+      {
+        desc: 'cook-breakfast.',
+        id: '1',
+      },
 
-storedTodo.forEach((todo) => UI.addBookTolist(todo));
-},
+      {
+        desc: 'attand-class.',
+        id: '2',
+      },
 
- static addBookTolist(todo){
-  const list = document.querySelector(.response)
-  list.innerHTML += `<li class="to-do-li">
-  <div class='todo-div'>
-  <input class="to-do-response" type="checkbox"
-  <p class ="todo-p">${todo.desc}
-  </p>
-  
-  </li
-  `;
+      {
+        desc: 'revise books.',
+        id: '3',
+      },
+      {
+        desc: 'visit granny.',
+        id: '3',
+      },
+    ];
+    storedTodo.forEach((todo) => UI.addBookToList(todo));
+  }
+
+  static addBookToList(todo) {
+    const list = document.querySelector('.js-list');
+    list.innerHTML += `
+      <li class='todo-li'>
+      <div class='todo-div'>
+      <input class='to-do-response' type='checkbox'>
+      <p class='todo-p-1'>${todo.desc}</p>
+      </div>
+      <i class="fa-solid fa-trash-can"></i>
+      </li>
+      `;
+  }
 }
-}
+
 export default UI;
