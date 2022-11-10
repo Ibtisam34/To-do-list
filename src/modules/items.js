c1e42cb7f77cb5999b5644c8080b78406f5fcc76
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-11-09 17:31:19 -0800

    added files

---
+++ src/modules/items.js
@@ -0,0 +1,25 @@
+const toDos = [
+  {
+    description: 'cook breakfast',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Attend class',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Revise books',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'visit granny',
+    completed: true,
+    index: 3,
+  },
+
+];
+
+export default toDos;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -7,15 +8,26 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'List-structure',
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-   clean: true,
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
