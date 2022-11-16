4c649c8953fc882ddae23a91c09ccbe85283f307
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-11-16 15:36:47 +0300

    added new

--- all.test.js
+++ all.test.js
@@ -1,27 +1,26 @@
 /**
  * @jest-environment jsdom
  */
- import { addTask, taskArr } from './src/modules/all';
- import deleteTask from './src/modules/remove';
- 
- describe('Add and Remove data', () => {
-   test('New item added', () => {
-     addTask({
-       index: 1,
-       description: 'Read book',
-       completed: true,
-     });
-     expect(taskArr).toHaveLength(0);
-   });
- 
-   test('New Task Deleted', () => {
-     // eslint-disable-next-line no-unused-expressions
-     (e) => {
-       const task = e.target.parentElement.parentElement;
-       deleteTask(task, e.target);
-     };
- 
-     expect(taskArr).toHaveLength(0);
-   });
- });
- 
+import { addTask, taskArr } from './src/modules/all.js';
+import deleteTask from './src/modules/remove.js';
+
+describe('Add and Remove data', () => {
+  test('New item added', () => {
+    addTask({
+      index: 1,
+      description: 'Read book',
+      completed: false,
+    });
+    expect(taskArr).toHaveLength(0);
+  });
+
+  test('New Task Deleted', () => {
+    // eslint-disable-next-line no-unused-expressions
+    (e) => {
+      const task = e.target.parentElement.parentElement;
+      deleteTask(task, e.target);
+    };
+
+    expect(taskArr).toHaveLength(0);
+  });
+});
