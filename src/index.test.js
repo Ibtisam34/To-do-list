46afc4b704ca2fc2996b637621c3b0981411f34a
unknown <ibtisam34.github.com>
2022-11-17 13:59:16 +0300

    added index test

--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,14 @@
-jest.mock('task.js');
-const $ = require('jquery');
-const updateTaskStatus = require('./modules/task');
+const updateTaskStatus = require('./modules/task.js');
 
-test('Edit a task after a click', () => {
+jest.mock('./modules/task.js', () => (el, task) => el);
+
+test('Update a task after a click', () => {
   // Set up our document body
-  document.body.innerHTML = `<li>
-   <div class="check">
-     <input type="checkbox" name="checkbox" class="checkbox" id="asdasd">
-     <input type="text" class="task-description" name="asdasd" id="task-name" value="asdasd">
-   </div>
-   <div class="actions">
-     <i class="fa-solid fa-pen-to-square edit"></i>
-     <i class="fa-solid fa-trash-can del"></i>
-   </div>
- </li>`;
+  const task = {
+    index: 1,
+    description: 'Read book',
+    completed: true,
+  };
 
-  // Tell the fetchCurrentUser mock function to automatically invoke
-  // its callback with some data
-  updateTaskStatus.mockImplementation((cb) => {
-    cb({});
-  });
-
-  // Use jquery to emulate a click on our button
-  $('.check').click();
-
-  // Assert that the fetchCurrentUser function was called, and that the
-  // #username span's inner text was updated as we'd expect it to.
-  expect(updateTaskStatus).toBeCalled();
-  expect(updateTaskStatus()).toHaveLength(0);
+  expect(updateTaskStatus('Read book', task)).toBe('Read book');
 });
